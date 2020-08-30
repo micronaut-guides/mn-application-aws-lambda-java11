b9391f82a1d00f041f4d75fdd324a7254cce75a4
Sergio del Amo <sergio.delamo@softamo.com>
2020-08-30 23:22:37 +0200

    initial commit

---
+++ complete/README.md
@@ -0,0 +1,4 @@
+## Feature aws-lambda documentation
+
+- [Micronaut AWS Lambda Function documentation](https://micronaut-projects.github.io/micronaut-aws/latest/guide/index.html#lambda)
+
---
+++ travis-build-pr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+export EXIT_STATUS=0
+
+echo "Executing tests for branch $TRAVIS_BRANCH"
+
+cd complete
+
+./test.sh || EXIT_STATUS=$?
+
+if [[ $EXIT_STATUS -ne 0 ]]; then
+  exit $EXIT_STATUS
+fi
+
+cd ..
+
+cd intermediate
+
+./test.sh || EXIT_STATUS=$?
+
+if [[ $EXIT_STATUS -ne 0 ]]; then
+  exit $EXIT_STATUS
+fi
+
+cd ..
+
+exit $EXIT_STATUS
