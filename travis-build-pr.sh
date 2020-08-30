5dd9767a3f89024c582016e08c0f4511c8d0b5de
Sergio del Amo <sergio.delamo@softamo.com>
2020-08-30 23:42:55 +0200

    fix travis-pr.sh

--- travis-build-pr.sh
+++ travis-build-pr.sh
@@ -3,26 +3,10 @@ set -e
 
 export EXIT_STATUS=0
 
-echo "Executing tests for branch $TRAVIS_BRANCH"
-
-cd complete
-
-./test.sh || EXIT_STATUS=$?
+./gradlew complete:test || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS -ne 0 ]]; then
   exit $EXIT_STATUS
 fi
-
-cd ..
-
-cd intermediate
-
-./test.sh || EXIT_STATUS=$?
-
-if [[ $EXIT_STATUS -ne 0 ]]; then
-  exit $EXIT_STATUS
-fi
-
-cd ..
 
 exit $EXIT_STATUS
